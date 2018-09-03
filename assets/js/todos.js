6e6ae9f6dcb603dfd9260b10cc15f86ce88d2716
Mike Bajor <michael.bajor@gmail.com>
2018-09-03 10:38:56 -0400

    Add ability to add, remove and mark as completed

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+// Check off specific todos by clicking
+$("ul").on("click","li",function(){
+    $(this).toggleClass("completed");
+});
+
+// Delete a todo item
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// Add todo
+$("input[type='text']").keypress(function(keyPressed){
+    if(keyPressed.which === 13){
+        // grab new todo text from input
+        var todoText = $(this).val();
+        // clear input
+        $(this).val("");
+        // create new li and add to ul
+        $("ul").append(`<li><span>X</span> ${todoText}</li>`);
+    }
+});
