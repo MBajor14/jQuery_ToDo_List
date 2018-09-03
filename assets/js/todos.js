92ea713c15c0a8a201d78d4222999a807ff57225
Mike Bajor <michael.bajor@gmail.com>
2018-09-03 11:36:15 -0400

    Add style and icons

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -19,6 +19,6 @@ $("input[type='text']").keypress(function(keyPressed){
         // clear input
         $(this).val("");
         // create new li and add to ul
-        $("ul").append(`<li><span>X</span> ${todoText}</li>`);
+        $("ul").append(`<li><span><i class="fa fa-trash"></i></span> ${todoText}</li>`);
     }
 });
